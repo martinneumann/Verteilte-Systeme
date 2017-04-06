62859455c1cefc726bd5e1866cc0fbb51024becf
Martin s65179 <s65179@beuth-hochschule.de>
2017-04-06 17:34:54 +0200

    Ende Uebung 1

---
+++ Aufg1/Client1.c
@@ -0,0 +1,47 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define PORT 4030
+
+int main() {
+
+	int sockfd, sfd, len;
+	ssize_t rc;
+	struct sockaddr_in myaddr, src_addr;
+	char puffer[1500];	
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	if (sockfd == -1) {
+		perror("Socketzuordnung!");
+		exit(1);
+	}	
+
+	memset(&myaddr, 0, sizeof(struct sockaddr_in));	
+	myaddr.sin_family = AF_INET;
+	myaddr.sin_port = htons(PORT);
+	rc = bind(sockfd, (struct sockaddr*) &myaddr, sizeof(struct sockaddr_in));
+	if (rc == -1) {
+		perror("Empfangsproblem");
+		exit(1);
+	}
+
+	/* Hier müssen wir die Kommunikation programmieren */
+	
+	while(1) {
+	
+		len = sizeof(struct sockaddr_in);
+		rc = sendto(sockfd, puffer, sizeof(puffer), 0, (struct serveraddr*)&myaddr, sizeof(sockaddr_in));				
+
+		close(sockfd);
+	}
+
+	
+
+	exit(0);
+
+}
